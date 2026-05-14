31aad7a372998ad3b6baa033254cf852f251f399
Vonce Chew <S10267888@connect.np.edu.sg>
2026-05-14 18:37:51 +0800

    Random Dice Mechanic
    Did up the barebones code for the dice mechanic

---
+++ Assets/Scripts/BattleMechanic/Dice.cs
@@ -0,0 +1,19 @@
+// Made by Vonce Chew
+using UnityEngine;
+
+public class Dice : MonoBehaviour
+{
+    // Variables
+    [HideInInspector]
+    public int DiceNumber;
+
+    /// <summary>
+    /// This function handles the dice roll mechanic
+    /// </summary>
+    public void DiceRoll()
+    {
+        // Plays dice roll animation
+
+        DiceNumber = UnityEngine.Random.Range(1, 7); // Returns random int between and including 1-6
+    }
+}
